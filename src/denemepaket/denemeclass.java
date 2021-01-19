4878bf4ee0293f4550dc8ecab333c1d99663e258
Zafer <zaferkok111@gmail.com>
2021-01-19 21:42:20 +0100

    2.deneme

--- src/denemepaket/denemeclass.java
+++ src/denemepaket/denemeclass.java
@@ -3,5 +3,6 @@ package denemepaket;
 public class denemeclass {
     public static void main(String[] args) {
         System.out.println("github giris");
+        System.out.println("github devam");
     }
 }
