2e393ff6ee11e5b8f7363dfcc7221c8f423be173
ZaferKok <62161849+ZaferKok@users.noreply.github.com>
2021-01-19 21:50:57 +0100

    Update denemeclass.java

--- src/denemepaket/denemeclass.java
+++ src/denemepaket/denemeclass.java
@@ -4,5 +4,6 @@ public class denemeclass {
     public static void main(String[] args) {
         System.out.println("github giris");
         System.out.println("github devam");
+        System.out.println("github hadi artık");
     }
 }
