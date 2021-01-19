3ae4c8a40f756acb285f53ee70d04a922d189160
Emrah Alpaslan <eailovemyjob@gmail.com>
2021-01-19 22:14:32 +0100

    emrah

--- src/denemepaket/denemeclass.java
+++ src/denemepaket/denemeclass.java
@@ -5,5 +5,7 @@ public class denemeclass {
         System.out.println("github giris");
         System.out.println("github devam");
         System.out.println("github hadi artık");
+        System.out.println("github emrah");
+
     }
 }
